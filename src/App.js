27659d914be4bf9d80a2e72559985d558b259a6b
gaoliang <gaoliangim@gmail.com>
2022-01-24 01:47:07 +0800

    feat: support click to active tab

--- src/App.js
+++ src/App.js
@@ -2,29 +2,30 @@
 import React from "react";
 import "./App.css";
 import { Tree } from "antd";
+import { Card } from "antd";
 import Icon from "@ant-design/icons";
+import { GithubOutlined } from '@ant-design/icons';
 
 function flatTreeToMap(roots) {
-  let result = {}
-  function each(nodes){
-    for(let node of nodes){
-     if(node.children){
-        result[node.id] = node
+  let result = {};
+  function each(nodes) {
+    for (let node of nodes) {
+      if (node.children) {
+        result[node.id] = node;
         each(node.children);
-     }
-   }
- }
- each(roots)
- return result;
+      }
+    }
+  }
+  each(roots);
+  return result;
 }
-
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       value: null,
-      treeData:  []
+      roots: [],
     };
   }
   componentDidMount() {
@@ -35,33 +36,52 @@ class App extends React.Component {
     let that = this;
     chrome.storage.local.get(["roots"], function (result) {
       let roots = result.roots;
-      let tabMap =  flatTreeToMap(roots)
-      chrome.tabs.query({}, tabs => {
-        tabs.forEach(tab => {
-          tabMap[tab.id].title = tab.title
-          tabMap[tab.id].icon = <Icon component={() => (<img src={tab.favIconUrl} alt="favicon" style={{width: '1rem', height: '1rem', display: 'inline-block'}}/>)} />
+      let tabMap = flatTreeToMap(roots);
+      chrome.tabs.query({}, (tabs) => {
+        tabs.forEach((tab) => {
+          tabMap[tab.id].title = tab.title;
+          tabMap[tab.id].icon = (
+            <Icon
+              component={() => (
+                <img
+                  src={tab.favIconUrl}
+                  alt="favicon"
+                  style={{
+                    width: "1rem",
+                    height: "1rem",
+                    display: "inline-block",
+                  }}
+                />
+              )}
+            />
+          );
         });
-        console.log("roots", roots)
-        console.log("tabMap", tabMap)
-        that.setState({
-          treeData: result.roots
-        })
-      })
+        console.log("roots", roots);
+        console.log("tabMap", tabMap);
+        that.setState({ roots,tabMap });
+      });
     });
   }
   render() {
     return (
       <div className="App">
-        { this.state.treeData.length > 0 ? 
-          <Tree
-            fieldNames = {{title: 'title', key: 'id', children: 'children'}}
-            showIcon
-            showLine = {{showLeafIcon: false}}
-            blockNode
-            defaultExpandAll
-            treeData={this.state.treeData}
-          />
-          : '暂无数据'}
+        <Card
+          title="Tab Tree"
+          extra={<a href="https://github.com/gaoliang/tab-tree"  target={'_blank'} rel="noreferrer"><GithubOutlined /></a>}
+          style={{ width: 600 }}
+        >
+          {this.state.roots.length > 0 ? (
+            <Tree
+              fieldNames={{ title: "title", key: "id", children: "children" }}
+              showIcon
+              showLine={{ showLeafIcon: false }}
+              blockNode
+              defaultExpandAll
+              treeData={this.state.roots}
+              onSelect={function(selectedKeys) {chrome.tabs.update(selectedKeys[0], {active: true})}}
+            />
+          ) : ( "暂无数据" )}
+        </Card>
       </div>
     );
   }
